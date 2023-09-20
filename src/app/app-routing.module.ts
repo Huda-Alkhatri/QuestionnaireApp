e9f826c510a3041f5a3220b1445b9cc51dfec72b
Huda-Alkhatri <hudalkhatri@outlook.com>
2023-09-20 15:22:34 +0400

    first comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserSelectorComponent } from './user-selector/user-selector.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ResultComponent } from './result/result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserSelectorComponent },
+  { path: 'question/:id', component: QuestionsComponent },
+  { path: 'result', component: ResultComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSelectorComponent } from './user-selector/user-selector.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ResultComponent } from './result/result.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSelectorComponent,
+    QuestionsComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, // Add this line
   ],
   providers: [],
   bootstrap: [AppComponent]
